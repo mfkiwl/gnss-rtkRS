339ec745c549f69b9796d0f2dec361902c42f1a7
Guillaume W. Bres <guillaume.bressaix@gmail.com>
2024-03-31 14:07:25 +0200

    interpolator obj
    Signed-off-by: Guillaume W. Bres <guillaume.bressaix@gmail.com>

--- src/interpolator.rs
+++ src/interpolator.rs
@@ -32,9 +32,9 @@ impl Interpolator {
         dy += (x_s - *before_x).to_seconds() / dx * after_y;
         Some(dy)
     }
-    fn dt(&self, x_j: Epoch) -> Option<(Epoch, Duration)> {
+    fn dt(&self) -> Option<(Epoch, Duration)> {
         if self.buffer.len() > 1 {
-            let (z2, _) = self.buffer.get(self.buffer.len() - 2)?;
+            let (z2, _) = self.buffer.get(0)?;
             let (z1, _) = self.buffer.last()?;
             Some((*z1, *z1 - *z2))
         } else {
@@ -44,10 +44,10 @@ impl Interpolator {
     /// Fill in buffer, which will always be optimized in size and
     /// evenly spaced (in time). Panic on chornological order mix up.
     pub fn fill(&mut self, x_j: Epoch, y_j: f64) {
-        if let Some((last, dt)) = self.dt(x_j) {
-            if (x_j - last).to_seconds().is_positive() {
+        if let Some((last, dt)) = self.dt() {
+            if (x_j - last).to_seconds().is_sign_positive() {
                 if (x_j - last) > dt {
-                    debug!("buffer reset on data gap");
+                    debug!("buffer reset on data gap @{:?}:{}", last, x_j - last);
                     self.buffer.clear();
                 }
                 self.buffer.push((x_j, y_j));
@@ -59,9 +59,18 @@ impl Interpolator {
         }
     }
     pub fn interpolate(&self, x_s: Epoch) -> Option<f64> {
-        match self.method {
-            Method::Linear => Self::linear_interp(x_s, &self.buffer),
-            //Method::Lagrange => lagrange_interp(self.x_k, self.y_k),
+        if let Some(y_s) = self
+            .buffer
+            .iter()
+            .filter_map(|(x, y)| if *x == x_s { Some(*y) } else { None })
+            .reduce(|k, _| k)
+        {
+            Some(y_s)
+        } else {
+            match self.method {
+                Method::Linear => Self::linear_interp(x_s, &self.buffer),
+                //Method::Lagrange => lagrange_interp(self.x_k, self.y_k),
+            }
         }
     }
     #[cfg(test)]
@@ -80,47 +89,52 @@ mod test {
         let mut interp = Interpolator::new(Method::Linear, 4);
         for (x_k, y_k, snapshot) in [
             (
-                Epoch::from_str("2020-01-01T00:00:00 UTC").unwrap(),
+                "2020-01-01T00:00:00 UTC",
                 1.0_f64,
-                vec![(Epoch::from_str("2020-01-01T00:00:00 UTC").unwrap(), 1.0_f64)],
+                vec![("2020-01-01T00:00:00 UTC", 1.0_f64)],
             ),
             (
-                Epoch::from_str("2020-01-01T00:00:30 UTC").unwrap(),
+                "2020-01-01T00:00:30 UTC",
                 2.0_f64,
                 vec![
-                    (Epoch::from_str("2020-01-01T00:00:00 UTC").unwrap(), 1.0_f64),
-                    (Epoch::from_str("2020-01-01T00:00:30 UTC").unwrap(), 2.0_f64),
+                    ("2020-01-01T00:00:00 UTC", 1.0_f64),
+                    ("2020-01-01T00:00:30 UTC", 2.0_f64),
                 ],
             ),
             (
-                Epoch::from_str("2020-01-01T00:10:30 UTC").unwrap(),
+                "2020-01-01T00:10:30 UTC",
                 3.0_f64,
-                vec![(Epoch::from_str("2020-01-01T00:10:30 UTC").unwrap(), 3.0_f64)],
+                vec![("2020-01-01T00:10:30 UTC", 3.0_f64)],
             ),
             (
-                Epoch::from_str("2020-01-01T00:10:35 UTC").unwrap(),
+                "2020-01-01T00:10:35 UTC",
                 4.0_f64,
                 vec![
-                    (Epoch::from_str("2020-01-01T00:10:30 UTC").unwrap(), 3.0_f64),
-                    (Epoch::from_str("2020-01-01T00:10:35 UTC").unwrap(), 4.0_f64),
+                    ("2020-01-01T00:10:30 UTC", 3.0_f64),
+                    ("2020-01-01T00:10:35 UTC", 4.0_f64),
                 ],
             ),
             (
-                Epoch::from_str("2020-01-01T00:10:40 UTC").unwrap(),
+                "2020-01-01T00:10:40 UTC",
                 5.0_f64,
                 vec![
-                    (Epoch::from_str("2020-01-01T00:10:30 UTC").unwrap(), 3.0_f64),
-                    (Epoch::from_str("2020-01-01T00:10:35 UTC").unwrap(), 4.0_f64),
-                    (Epoch::from_str("2020-01-01T00:10:40 UTC").unwrap(), 5.0_f64),
+                    ("2020-01-01T00:10:30 UTC", 3.0_f64),
+                    ("2020-01-01T00:10:35 UTC", 4.0_f64),
+                    ("2020-01-01T00:10:40 UTC", 5.0_f64),
                 ],
             ),
             (
-                Epoch::from_str("2020-01-01T00:12:00 UTC").unwrap(),
+                "2020-01-01T00:12:00 UTC",
                 6.0_f64,
-                vec![(Epoch::from_str("2020-01-01T00:12:00 UTC").unwrap(), 6.0_f64)],
+                vec![("2020-01-01T00:12:00 UTC", 6.0_f64)],
             ),
         ] {
+            let x_k = Epoch::from_str(x_k).unwrap();
             interp.fill(x_k, y_k);
+            let snapshot = snapshot
+                .iter()
+                .map(|(t, v)| (Epoch::from_str(t).unwrap(), *v))
+                .collect::<Vec<_>>();
             assert_eq!(interp.snapshot(), snapshot, "bad buffer logic");
         }
     }
@@ -129,11 +143,91 @@ mod test {
     fn chronological_mix_up() {
         let mut interp = Interpolator::new(Method::Linear, 4);
         for (x_k, y_k) in [
-            (Epoch::from_str("2020-01-01T00:00:00 UTC").unwrap(), 1.0_f64),
-            (Epoch::from_str("2020-01-01T00:00:30 UTC").unwrap(), 2.0_f64),
-            (Epoch::from_str("2020-01-01T00:00:00 UTC").unwrap(), 3.0_f64),
+            ("2020-01-01T00:00:00 UTC", 1.0_f64),
+            ("2020-01-01T00:00:30 UTC", 2.0_f64),
+            ("2020-01-01T00:00:00 UTC", 3.0_f64),
         ] {
+            let x_k = Epoch::from_str(x_k).unwrap();
             interp.fill(x_k, y_k);
+        }
+    }
+    #[test]
+    fn linear_basic() {
+        let mut interp = Interpolator::new(Method::Linear, 4);
+        for (x_k, y_k, x_s, expected) in [
+            (
+                "2020-01-01T00:00:00 UTC",
+                1.0_f64,
+                "2020-01-01T00:00:01 UTC",
+                None,
+            ),
+            (
+                "2020-01-01T00:00:30 UTC",
+                2.0_f64,
+                "2020-01-01T00:00:15 UTC",
+                Some(1.5),
+            ),
+            (
+                "2020-01-01T00:01:00 UTC",
+                3.0_f64,
+                "2020-01-01T00:00:15 UTC",
+                Some(1.5),
+            ),
+            (
+                "2020-01-01T00:01:00 UTC",
+                3.0_f64,
+                "2020-01-01T00:01:00 UTC",
+                Some(3.0),
+            ),
+        ] {
+            let x_k = Epoch::from_str(x_k).unwrap();
+            interp.fill(x_k, y_k);
+            let x_s = Epoch::from_str(x_s).unwrap();
+            assert_eq!(
+                interp.interpolate(x_s),
+                expected,
+                "wrong interpolation results"
+            );
+        }
+    }
+    #[test]
+    fn linear_advanced() {
+        let mut interp = Interpolator::new(Method::Linear, 16);
+        for (x_k, y_k) in [
+            ("2019-01-08T00:00:00 UTC", 0.391711350090E-04),
+            ("2019-01-08T00:00:30 UTC", 0.391710317949E-04),
+            ("2019-01-08T00:01:00 UTC", 0.391708385767E-04),
+            ("2019-01-08T00:01:30 UTC", 0.391709678221E-04),
+            ("2019-01-08T00:02:00 UTC", 0.391708653726E-04),
+            ("2019-01-08T00:02:00 UTC", 0.391708653726E-04),
+            ("2019-01-08T00:02:30 UTC", 0.391709273510E-04),
+            ("2019-01-08T00:03:00 UTC", 0.391708515569E-04),
+            ("2019-01-08T00:03:30 UTC", 0.391706625209E-04),
+        ] {
+            let x_k = Epoch::from_str(x_k).unwrap();
+            interp.fill(x_k, y_k);
+        }
+        for (x_s, y_s) in [
+            ("2019-01-08T00:03:30 UTC", 0.391706625209E-04),
+            (
+                "2019-01-08T00:01:33 UTC",
+                27.0 / 30.0 * 0.391709678221E-04 + 3.0 / 30.0 * 0.391708653726E-04,
+            ),
+            (
+                "2019-01-08T00:01:44 UTC",
+                16.0 / 30.0 * 0.391709678221E-04 + 14.0 / 30.0 * 0.391708653726E-04,
+            ),
+            (
+                "2019-01-08T00:01:57 UTC",
+                3.0 / 30.0 * 0.391709678221E-04 + 27.0 / 30.0 * 0.391708653726E-04,
+            ),
+        ] {
+            let x_s = Epoch::from_str(x_s).unwrap();
+            let y = interp.interpolate(x_s).expect(&format!(
+                "interpolation should have been feasible @{:?}",
+                x_s
+            ));
+            assert_eq!(y, y_s, "wrong interpolation results @{:?}", x_s);
         }
     }
 }
